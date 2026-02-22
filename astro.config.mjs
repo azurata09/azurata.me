ed5cfef1d7915f0f9c3c480703bd2cb84c41ccdd
azurata <git@azurata.me>
2026-02-23 06:34:52 +0900

    chore: デプロイ用の設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.azurata.me',
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en'],
